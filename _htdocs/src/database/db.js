702b013bf47b60f87f1190657ad63d4d76afa758
Marketing Pages <contato@julianamachado.dev>
2020-06-06 00:17:49 -0300

    Fiz o código para criar um registro na tabela chamada places, no db.js

--- _htdocs/src/database/db.js
+++ _htdocs/src/database/db.js
@@ -9,22 +9,43 @@ db.serialize(() => {
     //Usando comandos SQL para...
 
     //Criar uma tabela
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            //Colunas que terá na tabela places
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            name TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );
-    `)
+    db.run(`CREATE TABLE IF NOT EXISTS places (
+                //Colunas que terá na tabela places
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                image TEXT,
+                name TEXT,
+                address TEXT,
+                address2 TEXT,
+                state TEXT,
+                city TEXT,
+                items TEXT
+            );`);
 
     //Inserir dados na tabela
-    
+    const query = `INSERT INTO places(
+                        image,
+                        name,
+                        address,
+                        address2,
+                        state,
+                        city,
+                        items
+                    ) VALUES (?,?,?,?,?,?,?);`;
+    const values = ["../../_public/_image/colectoria.png",
+                    "Colectoria",
+                    "Guilherme Gemballa, Jardim América",
+                    "260",
+                    "Santa Catarina",
+                    "Rio Do Sul",
+                    "Resíduos Eletrônicos e Lâmpadas"];
+    function depoisDeInserirDados(err)  {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso!");
+        console.log(this);
+    }
+    db.run(query, values, depoisDeInserirDados)
 
     //Consultar dados na tabela
 
