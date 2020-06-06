337a713bd8ff722aa84bb41886e943ecb185bb69
Marketing Pages <contato@julianamachado.dev>
2020-06-06 00:02:30 -0300

    Fiz o código para criar uma tabela chamada places, no db.js

--- _htdocs/src/database/db.js
+++ _htdocs/src/database/db.js
@@ -5,4 +5,28 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./src/database/database.db");
 
 //Utilizar o db nas operações deste projeto
-//db.serialize
+db.serialize(() => {
+    //Usando comandos SQL para...
+
+    //Criar uma tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            //Colunas que terá na tabela places
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+
+    //Inserir dados na tabela
+    
+
+    //Consultar dados na tabela
+
+    //Excluir dados da tabela
+});
